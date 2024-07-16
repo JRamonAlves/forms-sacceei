fa9180f90b3584d42b0d4c2053a8e39eaa30ed7a
leofarias-cpu <l3onardo.almeid4@gmail.com>
2024-07-16 17:24:02 -0300

    Receba ip

--- app/api/historico.js
+++ app/api/historico.js
@@ -1,6 +1,6 @@
 export default async function acessaHistorico(req, res) {
   try {
-    const resposta = await fetch("localhost:8000/student/2023.2");
+    const resposta = await fetch("http://127.0.0.1:8000/student/2023.2");
     console.log(resposta.json())
     return resposta.body.json();
   } catch (err) {
