f0fb43ded48da805e885cbb507bbe945e9954800
leofarias-cpu <l3onardo.almeid4@gmail.com>
2024-07-18 15:34:01 -0300

    funfa

--- app/api/historico.js
+++ app/api/historico.js
@@ -1,12 +1,10 @@
 export default async function acessaHistorico(matricula) {
   try {
-    const resposta = await fetch("http://127.0.0.1:8000/student/2023.2", {
-      method : "POST",
-      headers : {
+    const resposta = await fetch("http://127.0.0.1:8000/studentHistory/2023.2?registration=122210093", {
+      method : "GET",
+      header : {
         'Content-type' : 'app/json'
-      },
-      body : JSON.stringify(matricula)
-
+      }
     });
     console.log(resposta.json());
     return resposta.body.json();
