6828b35e068320cfff76d45529b3ec3a7641232a
José Ramon Severo Alves <joseramonalves@gmail.com>
2024-07-24 19:09:52 -0300

    criando pagina das perguntas ainda

--- app/formularioPersonalizado/page.js
+++ app/formularioPersonalizado/page.js
@@ -3,15 +3,21 @@ import { useEffect, useState } from "react";
 import styles from "./../page.module.css";
 import { confereMatricula } from "../api/confereMatricula";
 import { perguntas } from "./perguntas";
-import { setLazyProp } from "next/dist/server/api-utils";
 
 export default function formsPessoal({ searchParams }) {
   const questions = perguntas;
   const [loading, changeLoading] = useState(true);
+  const [erro, changeErro] = useState(false);
   const [cadeiras, changeCadeiras] = useState([]);
 
   useEffect(() => {
     confereMatricula(searchParams.matricula).then((res) => {
+      if (!res) {
+        changeErro(true);
+        changeLoading(false);
+        return;
+      }
+
       const cursos = [];
       for (let index = 0; index < res.subjects.length; index++) {
         cursos.push(res.subjects[index].name);
@@ -24,8 +30,18 @@ export default function formsPessoal({ searchParams }) {
   if (loading) {
     return (
       <main className={styles.main}>
+        <div className={styles.divmassa}>Carregando...</div>
+      </main>
+    );
+  }
+
+  if (erro) {
+    return (
+      <main className={styles.main}>
         <div className={styles.divmassa}>
-          Buscando seus professores para voce falar mal deles ...
+          {
+            "Sua matricula esta errada, ou o site esta fora do ar. Espere alguns momentos e tente de novo :)"
+          }
         </div>
       </main>
     );
@@ -33,18 +49,50 @@ export default function formsPessoal({ searchParams }) {
 
   return (
     <main className={styles.main}>
-      <div className={styles.divmassa}>
+      <div className={styles.description}>
         Sobre os professores das cadeiras que voce pagou semestre passado,
         responda as seguintes questões:
       </div>
       <br />
-      <div className={styles.divmassa}>
-        {cadeiras.map((value, index) => {
+      <div className={styles.forms}>
+        <div className={styles.gridCadeiras}>
+          <label />
+          {cadeiras.map((value) => {
+            return <label>{value}</label>;
+          })}
+        </div>
+        {questions.map((value) => {
           return (
-            <div key={index} className={styles.code}>
-              <label>{value}</label>
+            <div className={styles.gridPerguntas}>
+              <label>{value.description}</label>
+              <label> </label>
+              {cadeiras.map(() => {
+                return (
+                  <div>
+                    <input type="radio" name="pergunta" value="sim" />
+                    <label>resposta</label>
+                    <br />
+
+                    <input type="radio" name="pergunta" value="sim" />
+                    <label>resposta</label>
+                    <br />
+
+                    <input type="radio" name="pergunta" value="sim" />
+                    <label>resposta</label>
+                    <br />
+
+                    <input type="radio" name="pergunta" value="sim" />
+                    <label>resposta</label>
+                    <br />
+
+                    <input type="radio" name="pergunta" value="sim" />
+                    <label>resposta</label>
+                    <br />
+                  </div>
+                );
+              })}
             </div>
-          )
+          );
         })}
       </div>
     </main>
